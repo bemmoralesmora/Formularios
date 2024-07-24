458fe84ee30b9f4c4fc34bc9f9469017858446c2
bemorales <bemorales@scl.edu.gt>
2024-07-24 08:07:06 -0600

    Diseño principal

---
+++ main.js
@@ -0,0 +1,31 @@
+// Selecciona el elemento con el id "root" del documento HTML
+let root = document.querySelector("#root");
+
+//Crea un nuevo elemento 'div_formularios' en el documento.
+let div_formularios = document.createElement("div");
+
+// Asigna una clase CSS llamada "div_formularios" al nuevo elemento 'div_formularios' para aplicar estilos.
+div_formularios.className = "div-formularios";
+
+//Se crea una lista de datos
+let formularios = [
+    {nombre : "login", link: "https"},
+    {nombre : "login", link: "https"},
+    {nombre : "login", link: "https"},
+    {nombre : "login", link: "https"}
+];
+
+//forEach por cada elemento de la lista va a dar una alerta.
+formularios.forEach(itemLista => {
+
+    let div_item = document.createElement("a");
+    div_item.className = "item";
+    div_item.href = itemLista.link;
+    div_item.innerText = itemLista.nombre;
+
+    div_formularios.appendChild(div_item);
+
+});
+
+//Añade el nodo root, el elemento div_formulario
+root.appendChild(div_formularios);
